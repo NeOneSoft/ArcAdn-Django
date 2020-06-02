8877293fe0a574a1ca7a760faee897db68e4f803
Gonzalo Romero <gonclapton@hotmail.com>
2020-06-02 14:29:40 -0500

    ADDED django-heroku

--- ArcAdn/settings.py
+++ ArcAdn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
@@ -178,3 +179,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILES_STORAGE = 'storages.backends.s3boto3.S3BOTO3Storage'
+django_heroku.settings(locals())
